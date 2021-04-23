6a87ac5fd3d8d595226f05e0ba44712d1c3e5c2a
SARomanchuk <SARomanchuk@sberbank.ru>
2021-04-23 20:30:06 +0300

    les 1 hw

--- parse_5ka.py
+++ parse_5ka.py
@@ -1,51 +1,62 @@
-import json
 import time
+import json
 from pathlib import Path
 import requests
-
-
-"""
-https://5ka.ru/special_offers/
-- Извлеч все доступные данные о товарах
-- Каждый товар положить в отдельный файл в формате JSON
-"""
+from urllib.parse import urlparse
 
 
 class Parse5ka:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0"
-    }
-    params = {
-        "records_per_page": 20,
-    }
+    headers = {"User-Agent": "FILIPP KIRKOROV"}
 
-    def __init__(self, star_url: str, save_path: Path):
-        self.star_url = star_url
+    def __init__(self, start_url: str, save_path: Path):
+        self.start_url = start_url
         self.save_path = save_path
 
-    def _get_response(self, url, *args, **kwargs):
+    def _get_response(self, url):
+        url = url.replace(urlparse(url).netloc, urlparse(self.start_url).netloc)
         while True:
-            response = requests.get(url, *args, **kwargs)
+            response = requests.get(url, headers=self.headers)
             if response.status_code == 200:
                 return response
-            time.sleep(3)
+            time.sleep(0.5)
 
     def run(self):
-        for product in self._parse(self.star_url):
-            file_path = self.save_path.joinpath(f"{product['id']}.json")
-            self._save(product, file_path)
+        for product in self._parse(self.start_url):
+            product_path = self.save_path.joinpath(f"{product['id']}.json")
+            self._save(product, product_path)
 
     def _parse(self, url: str):
         while url:
-            time.sleep(0.1)
-            response = self._get_response(url, headers=self.headers, params=self.params)
-            data = response.json()
+            response = self._get_response(url)
+            data: dict = response.json()
             url = data["next"]
             for product in data["results"]:
                 yield product
 
-    def _save(self, data: dict, file_path):
+    def _save(self, data: dict, file_path: Path):
         file_path.write_text(json.dumps(data, ensure_ascii=False))
+
+
+class CategoriesParser(Parse5ka):
+    def __init__(self, categories_url, *args, **kwargs):
+        self.categories_url = categories_url
+        super().__init__(*args, **kwargs)
+
+    def _get_categories(self):
+        response = self._get_response(self.categories_url)
+        data = response.json()
+        return data
+
+    def run(self):
+        for category in self._get_categories():
+            category["products"] = []
+            params = f"?categories={category['parent_group_code']}"
+            url = f"{self.start_url}{params}"
+
+            category["products"].extend(list(self._parse(url)))
+            file_name = f"{category['parent_group_code']}.json"
+            cat_path = self.save_path.joinpath(file_name)
+            self._save(category, cat_path)
 
 
 def get_save_path(dir_name):
@@ -56,7 +67,10 @@ def get_save_path(dir_name):
 
 
 if __name__ == "__main__":
-    save_path = get_save_path("products")
     url = "https://5ka.ru/api/v2/special_offers/"
-    parser = Parse5ka(url, save_path)
-    parser.run()
+    cat_url = "https://5ka.ru/api/v2/categories/"
+    save_path_products = get_save_path("products")
+    save_path_categories = get_save_path("categories")
+    parser_products = Parse5ka(url, save_path_products)
+    cat_parser = CategoriesParser(cat_url, url, save_path_categories)
+    cat_parser.run()
