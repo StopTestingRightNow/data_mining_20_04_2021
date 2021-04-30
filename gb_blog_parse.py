a1cf5e4b37729e7c3453a4b93d32b3fa2cd94c78
Stretop <stretop@gmail.com>
2021-04-30 19:46:37 +0300

    Lesson 2

--- gb_blog_parse.py
+++ gb_blog_parse.py
@@ -1,5 +1,6 @@
 import time
 import typing
+import hashlib
 
 import requests
 from urllib.parse import urljoin
@@ -7,16 +8,94 @@ from pymongo import MongoClient
 import bs4
 
 
+class ParseTask:
+    def __init__(self, url: str, start_url: str, callback: typing.Callable, *args, **kwargs):
+        self.start_url = start_url
+        self.time = time.time()
+        self.url = url
+        self.operation_requested = callback
+        self.soup = bs4.BeautifulSoup(self._get_response(url, *args, **kwargs).text, "lxml")
+        self.subtasks = set()
+
+    def __eq__(self, other):
+        self.url = other.url
+
+    def __hash__(self):
+        return int(hashlib.sha224(bytes(self.url, 'utf-8')).hexdigest(), 16)
+
+    def __call__(self, *args, **kwargs):
+        return self.operation_requested(url=self.url, soup=self.soup, *args, **kwargs)
+
+    def _get_response(self, url, *args, **kwargs):
+        if self.time + 0.9 < time.time():
+            time.sleep(0.5)
+        response = requests.get(url, *args, **kwargs)
+        self.time = time.time()
+        print(url)
+        return response
+
+    def _get_comments(self, post_id):
+        api_path = f"/api/v2/comments?commentable_type=Post&commentable_id={post_id}&order=desc"
+        response = self._get_response(urljoin(self.start_url, api_path))
+        data = response.json()
+        return data
+
+    def flatten_tasks(self):
+        result = self.subtasks
+        self.subtasks = set()
+        return result
+
+    def parse_feed(self, url, soup):
+        ul_pagination = soup.find("ul", attrs={"class": "gb__pagination"})
+        self.task_creator(url, ul_pagination.find_all("a"), self.parse_feed)
+        post_wrapper = soup.find("div", attrs={"class": "post-items-wrapper"})
+        self.task_creator(
+            url, post_wrapper.find_all("a", attrs={"class": "post-item__title"}), self.parse_post
+        )
+
+    def parse_post(self, url, soup):
+        author_tag = soup.find("div", attrs={"itemprop": "author"})
+        data = {
+            "post_data": {
+                "title": soup.find("h1", attrs={"class": "blogpost-title"}).text,
+                "url": url,
+                "id": soup.find("comments").attrs.get("commentable-id"),
+            },
+            "author_data": {
+                "url": urljoin(url, author_tag.parent.attrs.get("href")),
+                "name": author_tag.text,
+            },
+            "tags_data": [
+                {"name": tag.text, "url": urljoin(url, tag.attrs.get("href"))}
+                for tag in soup.find_all("a", attrs={"class": "small"})
+            ],
+            "comments_data": self._get_comments(soup.find("comments").attrs.get("commentable-id")),
+        }
+        return data
+
+    def task_creator(self, url, tags_list, callback):
+        links = set(
+            urljoin(url, itm.attrs.get("href")) for itm in tags_list if itm.attrs.get("href")
+        )
+        for link in links:
+            task = self.get_task(link, callback)
+            self.subtasks.add(task)
+
+    def get_task(self, url: str, callback: typing.Callable) -> typing.Callable:
+        task = ParseTask(url, self.start_url, callback)
+        return task(url)
+
+
 class GbBlogParse:
     def __init__(self, start_url, collection):
         self.time = time.time()
         self.start_url = start_url
         self.collection = collection
-        self.done_urls = set()
-        self.tasks = []
-        start_task = self.get_task(self.start_url, self.parse_feed)
-        self.tasks.append(start_task)
-        self.done_urls.add(self.start_url)
+        self.done_tasks = set()
+        self.tasks = set()
+        start_task = ParseTask(self.start_url, self.start_url, self.parse_feed)
+        self.tasks.add(start_task)
+        self.done_tasks.add(start_task)
 
     def _get_response(self, url, *args, **kwargs):
         if self.time + 0.9 < time.time():
@@ -31,14 +110,9 @@ class GbBlogParse:
         return soup
 
     def get_task(self, url: str, callback: typing.Callable) -> typing.Callable:
-        def task():
-            soup = self._get_soup(url)
-            return callback(url, soup)
-
-        if url in self.done_urls:
-            return lambda *_, **__: None
-        self.done_urls.add(url)
-        return task
+        task = ParseTask(url, self.start_url, callback)
+        soup = self._get_soup(url)
+        return task()
 
     def task_creator(self, url, tags_list, callback):
         links = set(
@@ -46,7 +120,7 @@ class GbBlogParse:
         )
         for link in links:
             task = self.get_task(link, callback)
-            self.tasks.append(task)
+            self.tasks.add(task)
 
     def parse_feed(self, url, soup):
         ul_pagination = soup.find("ul", attrs={"class": "gb__pagination"})
