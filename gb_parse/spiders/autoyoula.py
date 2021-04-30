20a8068d2201e03a188561ffa17031ee23031a7e
SARomanchuk <SARomanchuk@sberbank.ru>
2021-04-30 22:21:39 +0300

    les 4

---
+++ gb_parse/spiders/autoyoula.py
@@ -0,0 +1,37 @@
+import scrapy
+
+
+class AutoyoulaSpider(scrapy.Spider):
+    name = "autoyoula"
+    allowed_domains = ["auto.youla.ru"]
+    start_urls = ["https://auto.youla.ru/"]
+
+    def _get_follow(self, response, selector_str, callback):
+        for itm in response.css(selector_str):
+            url = itm.attrib["href"]
+            yield response.follow(url, callback=callback)
+
+    def parse(self, response, *args, **kwargs):
+        yield from self._get_follow(
+            response,
+            ".TransportMainFilters_brandsList__2tIkv .ColumnItemList_column__5gjdt a.blackLink",
+            self.brand_parse,
+        )
+
+    def brand_parse(self, response):
+        yield from self._get_follow(
+            response, ".Paginator_block__2XAPy a.Paginator_button__u1e7D", self.brand_parse
+        )
+        yield from self._get_follow(
+            response,
+            "article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu.blackLink",
+            self.car_parse,
+        )
+
+    def car_parse(self, response):
+        # print(1)
+        # data = {
+        #     "url": response.url,
+        #     "title": response.css(".AdvertCard_advertTitle__1S1Ak::text").extract_first(),
+        # }
+        pass
