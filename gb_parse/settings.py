07a81778ba769dd7f84dc528b5a8d78f14b0db34
SARomanchuk <SARomanchuk@sberbank.ru>
2021-05-11 22:19:01 +0300

    les 7

--- gb_parse/settings.py
+++ gb_parse/settings.py
@@ -53,11 +53,11 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    # 'gb_parse.middlewares.GbParseDownloaderMiddleware': 543,
-    "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
-    "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     # 'gb_parse.middlewares.GbParseDownloaderMiddleware': 543,
+#     "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
+#     "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
