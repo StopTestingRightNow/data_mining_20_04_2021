ebdd7bdba52fdc44dc4f4327a3c2bbb8e0a41c3c
Stretop <stretop@gmail.com>
2021-05-21 18:00:35 +0300

    Coursework

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from instagrapi import Client
 def get_mut_following(init_username: str, client: Client, amount=20):
 
     user_id = cl.user_id_from_username(init_username)
-    followers_dict = client.user_followers(user_id, amount=20)
-    following_dict = client.user_following(user_id, amount=20)
+    followers_dict = client.user_followers(user_id, amount=amount)
+    following_dict = client.user_following(user_id, amount=amount)
 
     followers_set = set()
     following_set = set()
@@ -21,7 +21,7 @@ def get_mut_following(init_username: str, client: Client, amount=20):
     return followers_set.intersection(following_set)
 
 
-def find_chain(start_username: str, finish_username: str, client: Client, steps_limit=6):
+def find_chain(start_username: str, finish_username: str, client: Client, steps_limit=6, followers_limit=20):
 
     users_passed_set = set()
     users_working_set = {start_username, }
@@ -29,7 +29,7 @@ def find_chain(start_username: str, finish_username: str, client: Client, steps_
     for i in range(1, steps_limit):
         next_step_set = set()
         for username in users_working_set:
-            new_batch = get_mut_following(username, client=cl, amount=20)
+            new_batch = get_mut_following(username, client=cl, amount=followers_limit)
             if finish_username in new_batch:
                 return i
             next_step_set.update(new_batch)
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     cl = Client()
     cl.login(username, password)
 
-    result = find_chain("ligapodlecov1", "ginstaboy", client=cl, steps_limit=6)
+    result = find_chain("ligapodlecov1", "ginstaboy", client=cl, steps_limit=6, followers_limit=30)
 
     if result is not None:
         print(f"Chain found in {result} steps")
